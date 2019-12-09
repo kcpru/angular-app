1ddb361525dc1c0c450bc8e18c4fa459f1102ae4
Kacper Ślusarczyk <kacper12345678909@gmail.com>
2019-12-09 09:21:29 +0100

    Updates

---
+++ precache-manifest.56a02048ed864f5e73f841bf18e1c0a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7086097ccfb7668b78a33b06b62cb9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bacccaf8b31585726e6f",
+    "url": "/static/css/main.ca59ddad.chunk.css"
+  },
+  {
+    "revision": "adc1676d0a38cee47724",
+    "url": "/static/js/2.71fdf400.chunk.js"
+  },
+  {
+    "revision": "bacccaf8b31585726e6f",
+    "url": "/static/js/main.3c24ec95.chunk.js"
+  },
+  {
+    "revision": "e5286344d3012f92b681",
+    "url": "/static/js/runtime-main.f6a57df3.js"
+  },
+  {
+    "revision": "9d5229ffcf1d18e4b168037955b3b389",
+    "url": "/static/media/clear.9d5229ff.png"
+  },
+  {
+    "revision": "89566184fef4ac81743ccd83ba1582ae",
+    "url": "/static/media/clouds.89566184.png"
+  },
+  {
+    "revision": "f8e78c972339988aff2427012cf687cc",
+    "url": "/static/media/rain.f8e78c97.png"
+  }
+]);
